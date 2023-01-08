96305cf568ea1629afc6781827ef841f1bf009d8
William Desportes <williamdes@wdes.fr>
2023-01-08 19:11:57 +0400

    Move to /dashboard.html and add header and footer search inputs

--- debian.dashboard.air-balloon.cloud/assets/js/main.js
+++ debian.dashboard.air-balloon.cloud/assets/js/main.js
@@ -1,3 +1,34 @@
-$(document).ready( function () {
-    $('#packages').DataTable();
+$(document).ready(function () {
+    // Setup - add a text input to each footer cell
+    $('#packages tfoot th').each(function () {
+        var title = $(this).text();
+        $(this).html('<input class="form-control" style="width: min-content;" type="text" placeholder="Search ' + title + '" />');
+    });
+    $('#packages thead th').each(function () {
+        var title = $(this).text();
+        $(this).html('<input class="form-control" style="width: min-content;" type="text" placeholder="Search ' + title + '" />');
+    });
+ 
+    // DataTable
+    var table = $('#packages').DataTable({
+        initComplete: function () {
+            // Apply the search
+            this.api()
+                .columns()
+                .every(function () {
+                    var that = this;
+ 
+                    $('input', this.footer()).on('keyup change clear', function () {
+                        if (that.search() !== this.value) {
+                            that.search(this.value).draw();
+                        }
+                    });
+                    $('input', this.header()).on('keyup change clear', function () {
+                        if (that.search() !== this.value) {
+                            that.search(this.value).draw();
+                        }
+                    });
+                });
+        },
+    });
 });
